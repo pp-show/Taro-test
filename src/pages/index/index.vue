808c92599cc0ce732832fa0ac03dcaf1e3dff52e
krystalpan <krystalpan@tencent.com>
2021-09-13 20:47:40 +0800

    ceshi

---
+++ src/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <view class="index">
+    <NumberDisplay />
+    <NumberSubmit />
+    <!-- <input /> -->
+  </view>
+</template>
+
+<script>
+import NumberDisplay from "../../components/NumberDisplay.vue";
+import NumberSubmit from "../../components/NumberSubmit.vue";
+
+export default {
+  name: "Index",
+  components: {
+    NumberDisplay,
+    NumberSubmit,
+  },
+};
+</script>
+
+<style>
+.index {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
